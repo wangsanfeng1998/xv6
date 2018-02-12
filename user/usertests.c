3c6b748e6951dd23fa713875d512db6db5e94ee1
Sanfeng Wang <swx5979@tlab-14.ece.northwestern.edu>
2018-02-12 16:32:40 -0600

    added extra credit tests

--- user/usertests.c
+++ user/usertests.c
@@ -194,6 +194,75 @@ whenProcessExists_countReturns0()
 }
 
 void
+whenNotUsingSharedMemory_ProcessCanAllocateAllSpaceUpToUsertop()
+{
+  printf(1, "Test: whenNotUsingSharedMemory_ProcessCanAllocateAllSpaceUpToUsertop...");
+  char* startAddress = sbrk(0);
+  uint bytesRemaining = (160 * 4096) - (uint)startAddress - 4096;  // subtract 4096 for the 0th page
+  char* sbrkResult = sbrk(bytesRemaining);
+
+  if(sbrkResult == (char*)0xffffffff){
+    testFailed();
+    printf(1, "      Failed to use full user address space for normal memory.\n");
+    expectedVersusActualNumeric("'sbrkResult'", 4096*159, (int)sbrkResult);
+    return;
+  }
+
+  testPassed();
+}
+
+void
+whenUsingOneSharedPage_ProcessCanAllocateAllRemainingSpace()
+{
+  printf(1, "Test: whenUsingOneSharedPage_ProcessCanAllocateAllRemainingSpace...");
+  char* startAddress = sbrk(0);
+  char* sharedPage = shmem_access(0);
+  sharedPage = sharedPage + 0;
+  uint bytesAvailable = (160 * 4096) - (uint)startAddress - 2 * 4096; // subtract 2 * 4096 for 0th page and shared page
+  char* sbrkResult = sbrk(bytesAvailable);
+
+  if(sbrkResult == (char*)0xffffffff) {
+    testFailed();
+    printf(1, "      Failed to use 1 page for shared mem and 158 for normal memory.\n");
+    return;
+  }
+
+  testPassed();
+}
+
+void
+whenUsingAllButTwoPagesForNormalMemory_ProcessCanAllocateTwoSharedPages()
+{
+  printf(1, "Test: whenUsingAllButTwoPagesForNormalMemory_ProcessCanAllocateTwoSharedPages...");
+  char* startAddress = sbrk(0);
+  uint bytesToAllocate = (160 * 4096) - (uint)startAddress - 3 * 4096; // subtract 3 * 4096 for 0th page and 2 shared pages
+  char* sbrkResult = sbrk(bytesToAllocate);
+  char* sharedPage0 = shmem_access(0);
+  char* sharedPage4 = shmem_access(3);
+
+  if(sbrkResult == (char*)0xffffffff) {
+    testFailed();
+    printf(1, "      Failed to allocate normal memory.\n");
+    return;
+  }
+
+  if(sharedPage0 == NULL) {
+    testFailed();
+    printf(1, "      Failed to allocate first shared page.\n");
+    return;
+  }
+
+  if(sharedPage4 == NULL) {
+    testFailed();
+    printf(1, "      Failed to allocate second shared page.\n");
+    return;
+  }
+
+  testPassed();
+  
+}
+
+void
 beforeRequestingSharedMemory_countReturns0()
 {
   printf(1, "Test: beforeRequestingSharedMemory_countReturns0...");
@@ -1727,6 +1796,34 @@ main(int argc, char *argv[])
     exit();
   }
   wait();
+  
+  if(pid == 0){
+    whenNotUsingSharedMemory_ProcessCanAllocateAllSpaceUpToUsertop();
+    exit();
+  }
+  wait();
+
+  pid = fork();
+  if(pid == 0){
+    whenUsingOneSharedPage_ProcessCanAllocateAllRemainingSpace();
+    exit();
+  }
+  wait();
+  
+  pid = fork();
+  if(pid == 0){
+    whenUsingAllButTwoPagesForNormalMemory_ProcessCanAllocateTwoSharedPages();
+    exit();
+  }
+  wait();
+
+  pid = fork();
+  if(pid == 0){
+    whenSharingAPage_ParentSeesChangesMadeByChild();
+    exit();
+  }
+  wait();
+  
   
   /*
   printf(1, "usertests starting\n");
